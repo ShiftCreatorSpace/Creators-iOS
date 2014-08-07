1bf6957cb0ed183c5c80c4124c0417b69a15f5db
Natasja Nielsen <natmnielsen@gmail.com>
2014-08-06 23:27:41 -0700

    Added sorted member search by first/last name

--- creators/AnnouncementViewController.swift
+++ creators/AnnouncementViewController.swift
@@ -21,14 +21,45 @@ class AnnouncementViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         titleLabel.text = String(announcement["title"] as NSString)
         details.text = String(announcement["details"] as NSString)
+        
+        //println(details.frame)
+        var frame: CGRect = details.frame
+        frame.size.height = details.contentSize.height;
+        details.frame = frame
+        //println(details.frame)
+        
+        
+        //println(self.image.size.width)
+        //println(self.image.size.height)
+        
+        photo.frame = CGRectMake(photo.frame.origin.x, photo.frame.origin.y, self.image.size.width, self.image.size.height)
+        photo.contentMode = UIViewContentMode.ScaleAspectFit
+        //        photo.frame.size.height = image.size.height
         photo.image = self.image
+        
+        
+        //println(photo.frame)
+        //println(photo.image.size.width)
+        //println(photo.image.size.height)
         //if (photo) {
             //photo!.image = announcement["photo"] as UIImage
             
             //photo.image = photo.image
             //println(photo)
         //}
+//        UIImage * img = [UIImage imageNamed:@"someImage.png"];
+//        CGSize imgSize = img.size;
         
+        //calculate scale ratio on width
+        
+//        float ratio=yourImageView.frame.size.width/imgSize.width;
+        //check scaled height (using same ratio to keep aspect)
+        
+//        float scaledHeight=imgSize.height*ratio;
+//        if(scaledHeight < yourImageView.frame.size.height)
+//        {
+            //update height of your imageView frame with scaledHeight
+//        }
         //titleLabel.text = self.album?.title
         //albumCover.image = UIImage(data: NSData(contentsOfURL: NSURL(string: self.album?.largeImageURL)))
     }
--- creators/MembersViewController.swift
+++ creators/MembersViewController.swift
@@ -8,10 +8,17 @@
 
 import Foundation
 
-class MembersViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
+class MembersTableViewCell: UITableViewCell {
+    init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: UITableViewCellStyle.Default, reuseIdentifier: reuseIdentifier)
+    }
+}
+
+class MembersViewController: UITableViewController, UISearchDisplayDelegate, UISearchBarDelegate {
     // define the class
     var membersData: NSArray = []
     var membersPhotos = Dictionary<String, PFImageView>()
+    var searchResults: NSArray = []
     
     var lastDescriptor: NSSortDescriptor = NSSortDescriptor(key: "lastName", ascending: true, selector: "caseInsensitiveCompare:")
     var firstDescriptor: NSSortDescriptor = NSSortDescriptor(key: "firstName", ascending: true, selector: "caseInsensitiveCompare:")
@@ -28,7 +35,7 @@ class MembersViewController: UITableViewController, UITableViewDelegate, UITable
                 self.membersData = objects
                 self.membersData = self.membersData.sortedArrayUsingDescriptors([self.lastDescriptor, self.firstDescriptor])
             
-                for element : AnyObject in self.membersData {
+                for element: AnyObject in self.membersData {
                     if let member = element as? PFUser  {
                         var selfie: PFImageView = PFImageView()
                         selfie.image = UIImage(named: "square")
@@ -40,13 +47,16 @@ class MembersViewController: UITableViewController, UITableViewDelegate, UITable
                                 self.tableView.reloadData()
                             }
                         })
-                        self.membersPhotos.updateValue(selfie, forKey: toString(member["username"]))
+                        self.membersPhotos.updateValue(selfie, forKey: member.objectId)
                         //println(element)
                     }
                 }
                 self.tableView.reloadData()
             }
         })
+        self.searchDisplayController.delegate = self
+        self.searchDisplayController.searchResultsDataSource = self
+        self.searchDisplayController.searchResultsDelegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -61,39 +71,94 @@ class MembersViewController: UITableViewController, UITableViewDelegate, UITable
     }*/
     
     override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return self.membersData.count
+        if tableView == self.searchDisplayController.searchResultsTableView {
+            return self.searchResults.count
+        } else {
+            return self.membersData.count
+        }
+    }
+    
+    override func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        return 80
     }
     
     override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         //let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("memberCell", forIndexPath: indexPath) as MembersTableViewCell
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("memberCell", forIndexPath: indexPath) as UITableViewCell
-            
-        if self.membersData.count > 0 {
-            let member = self.membersData.objectAtIndex(indexPath.row) as PFUser
-            let firstName = String(member["firstName"] as NSString)
-            let lastName = String(member["lastName"] as NSString)
-            let selfie = self.membersPhotos[toString(member["username"])]!.image
+        var member : PFUser?
+        
+        if (tableView == self.searchDisplayController.searchResultsTableView && self.searchResults.count > 0) {
+            member = self.searchResults.objectAtIndex(indexPath.row) as? PFUser
+            var name = member!["firstName"] as NSString
+        } else {
+            if self.membersData.count > 0 {
+                member = self.membersData.objectAtIndex(indexPath.row) as? PFUser
+            }
+        }
+        
+        if member {
+            let firstName = String(member!["firstName"] as NSString)
+            let lastName = String(member!["lastName"] as NSString)
+            let selfie = self.membersPhotos[member!.objectId]!.image
             
             cell.textLabel.text = firstName + " " + lastName
             cell.imageView.image = selfie
-            
-            //println(firstName)
-            //println(self.membersPhotos[toString(member["username"])])
         }
         return cell
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject) {
         var memberViewController: MemberViewController = segue.destinationViewController as MemberViewController
-        var memberIndex = tableView!.indexPathForSelectedRow().row
-        var selectedMember = self.membersData.objectAtIndex(memberIndex) as PFUser
+        var memberIndex = NSInteger()
+        var selectedMember = PFUser()
+        
+        if (self.searchDisplayController.active == true) {
+            memberIndex = self.searchDisplayController.searchResultsTableView!.indexPathForSelectedRow().row
+            selectedMember = self.searchResults.objectAtIndex(memberIndex) as PFUser
+        } else {
+            memberIndex = self.tableView!.indexPathForSelectedRow().row
+            selectedMember = self.membersData.objectAtIndex(memberIndex) as PFUser
+        }
+
         memberViewController.member = selectedMember
-        memberViewController.image = self.membersPhotos[toString(selectedMember["username"])]!.image
+        memberViewController.image = self.membersPhotos[selectedMember.objectId]!.image
     }
-/*
+    
+    func filterContentForSearchText(searchText: NSString!) {
+        //  Use NSPredicate after move to Core Data
+        //let resultPredicate = NSPredicate(format: "name contains[c] %@", searchText)
+        //searchResults = membersData.filteredArrayUsingPredicate(resultPredicate)
+        
+        // var query: PFQuery = PFUser.query()
+        //query.whereKey("firstName", matchesRegex: NSString(format: "(?i:^.*%@.*$)", searchText))
+        //query.whereKey("lastName", matchesRegex: NSString(format: "(?i:^.*%@.*$)", searchText))
+        
+        var queryFirst: PFQuery = PFUser.query()
+        var queryLast: PFQuery = PFUser.query()
+        queryFirst.whereKey("firstName", matchesRegex: NSString(format: "(?i:^.*%@.*$)", searchText))
+        queryLast.whereKey("lastName", matchesRegex: NSString(format: "(?i:^.*%@.*$)", searchText))
+        var queries: NSArray = [queryFirst, queryLast]
+        var query: PFQuery = PFQuery.orQueryWithSubqueries(queries)
+        
+        query.findObjectsInBackgroundWithBlock({(NSMutableArray objects, NSError error) in
+            if (error) {
+                NSLog("error " + error.localizedDescription)
+            } else {
+                self.searchResults = objects
+                self.searchDisplayController.searchResultsTableView.reloadData()
+            }
+        })
+    }
+
+    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: NSString!) -> Bool {
+        self.filterContentForSearchText(searchString)
+        return true
+    }
+
     override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
-        self.performSegueWithIdentifier("memberSegue", sender: self)
+        //self.performSegueWithIdentifier("memberSegue", sender: self)
+        self.searchDisplayController.setActive(false, animated: true)
     }
-*/
+
 }
